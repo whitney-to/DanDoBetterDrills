20941b10e7797a084b7734f3f100b68003ad4a7f
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-02-28 16:29:25 -0500

    modify return value for add method in MathUtility class

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -11,7 +11,7 @@ public class MathUtilities {
      * @return sum of `baseValue` and `difference`
      */
     public Integer add(int baseValue, int difference) {
-        return null;
+        return baseValue+difference;
     }
 
     /**
