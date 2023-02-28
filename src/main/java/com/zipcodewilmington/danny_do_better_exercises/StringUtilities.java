2c99228161d67931c942ed006ca2563ebc4ac3c9
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-02-28 17:19:20 -0500

    modified concatenation, getPrefix, getSuffix in StringUtilities

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -8,7 +8,7 @@ public class StringUtilities {
      * @return `Hello World` as a string
      */
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
 
     /**
@@ -17,7 +17,8 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+
+        return firstSegment+secondSegment;
     }
 
     /**
@@ -26,7 +27,8 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+
+        return firstSegment+secondSegment;
     }
 
     /**
@@ -34,7 +36,8 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+
+        return input.substring(0,3);
     }
 
     /**
@@ -42,7 +45,12 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+        String result = "";
+        int endIndex = input.length() - 1;
+        for(int i = endIndex; i > endIndex - 3; i--){
+            result+=input.charAt(i);
+        }
+        return result;
     }
 
     /**
